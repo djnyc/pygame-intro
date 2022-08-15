59a4a2c543faa273c836150efbc87c4b89e63202
Daryl Noyce <daryl.noyce@highgateschool.org.uk>
2022-08-15 11:23:24 +0100

    Some stuff

--- python-platform.py
+++ python-platform.py
@@ -9,6 +9,10 @@ screen = pygame.display.set_mode((800, 600))
 
 pygame.display.set_caption("Platform")
 
+cx, cy = 400, 300
+
+player = pygame.Rect(400, 300, 20, 20)
+platform = pygame.Rect(0, 500, 800, 40)
 
 while True:
     if pygame.event.get(pygame.QUIT):
@@ -16,15 +20,27 @@ while True:
 
     keys = pygame.key.get_pressed()
 
+    ox, oy = cx, cy
+
     if keys[pygame.K_UP]:
-        print("Up")
+        cy -= 1
     if keys[pygame.K_DOWN]:
-        print("Down")
+        cy += 1
     if keys[pygame.K_LEFT]:
-        print("Left")
+        cx -= 1
     if keys[pygame.K_RIGHT]:
-        print("Right")
+        cx += 1
 
     screen.fill((100, 100, 100))
+
+    player.topleft = cx, cy
+
+    if player.colliderect(platform):
+        cx, cy = ox, oy
+        player.topleft = cx, cy
+
+    pygame.draw.rect(screen, (100, 0, 100), player)
+    pygame.draw.rect(screen, (100, 100, 0), platform)
+
 
     pygame.display.flip()
